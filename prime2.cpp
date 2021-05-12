5566ce8f11d45ae3be483328dc2e8108718a3975
suma88966 <84056815+suma88966@users.noreply.github.com>
2021-05-12 10:32:43 +0530

    Add files via upload

---
+++ prime2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	{
+	int n,i,status=0;
+	cout<<"enter a number";
+	cin>>n;
+	for(i=2;i<=n/2;i++)
+	{
+		if(n%i==0)
+		{
+		 cout<<"not a prime";
+		 status = 1;
+		 break;
+	    }
+	}
+	if(status!=1)
+	cout<<"prime";
+}
+}
