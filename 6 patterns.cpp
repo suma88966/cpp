82d8368638ccd6711e57d53055cc1b4f9be7777f
scode88966 <84056815+suma88966@users.noreply.github.com>
2021-05-12 17:39:30 +0530

    Add files via upload

---
+++ 6 patterns.cpp
@@ -0,0 +1,139 @@
+ /*  *
+    **
+   ***
+  ****
+ *****
+ */
+#include<iostream>
+using namespace std;
+int main()
+{  
+
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5-i;j++)
+	       cout<<" ";
+	    for(int k=0;k<=i;k++)
+	        cout<<"*";
+	    cout<<endl;
+	}
+}
+/*
+
+*
+**
+***
+****
+*****
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	for(int i=0;i<5;i++)
+	{
+	    for(int k=0;k<=i;k++)
+	        cout<<"*";
+	    cout<<endl;
+	}
+}
+
+
+/*
+
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5-i;j++)
+		   cout<<" ";
+		for(int k=0;k<=i;k++)
+		   cout<<"* ";
+		cout<<endl;
+		
+	}
+}
+/*
+ * * * * *
+  * * * *
+   * * *
+    * *
+     *
+
+*/
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<=i;j++)
+		   cout<<" ";
+		for(int k=0;k<5-i;k++)
+		   cout<<"* ";
+		cout<<endl;
+		
+	}
+}
+
+
+/*
+
+*****
+****
+***
+**
+*
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for(int i=0;i<5;i++)
+	{
+		for(int k=0;k<5-i;k++)
+		   cout<<"*";
+		cout<<endl;
+		
+	}
+}
+
+*/
+
+/*
+ *****
+  ****
+   ***
+    **
+     *
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<=i;j++)
+		   cout<<" ";
+		for(int k=0;k<5-i;k++)
+		   cout<<"*";
+		cout<<endl;
+		
+	}
+}
