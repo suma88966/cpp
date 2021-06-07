d951aec431f72b13d5e1cf45ae5596880f35fbb1
scode88966 <84056815+suma88966@users.noreply.github.com>
2021-06-07 09:55:50 +0530

    Add files via upload

---
+++ dp fibo.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int dp[1000005];
+int fact(int n)
+{
+	if(n==1)
+	return 1;
+	if(dp[n]!=-1)
+	{
+		return dp[n];
+	}
+	return dp[n]=n*fact(n-1);
+}
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=0;i<=n;i++)
+	{
+		dp[i]=-1;
+	}
+	cout<<fact(n)<<endl;
+	return 0;
+}
